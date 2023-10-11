e9e40e80dcfa563819f8d65d3f655a945a71b324
Jean <jean@jlouis.me>
2023-10-11 09:33:46 +0100

    Randomize library state

--- src/library.ts
+++ src/library.ts
@@ -1,6 +1,31 @@
 import { Book } from './book';
 import * as fs from 'fs';
 
+const RANDOM_REFERENCE_COUNT = 25;
+const RANDOM_BORROWED_COUNT = 5;
+
+function generateRandomNumbersArray(
+  amount = 0,
+  max = 10,
+  excludedIndexes = [],
+) {
+  if (!amount) {
+    return [];
+  }
+
+  const numbers: Set<number> = new Set();
+  const excluded: Set<number> = new Set(excludedIndexes);
+
+  while (numbers.size < amount) {
+    const randomNumber = Math.floor(Math.random() * max) + 1;
+    if (!excluded.has(randomNumber)) {
+      numbers.add(randomNumber);
+    }
+  }
+
+  return [...numbers];
+}
+
 export class Library {
   private books: Book[] = [];
 
@@ -13,5 +38,38 @@ export class Library {
   private initializeLibraryFromFile() {
     const data = fs.readFileSync(this.fileName, 'utf-8');
     this.books = JSON.parse(data);
+  }
+
+  // Set random books as reference and borrowed
+  // Assumption: a reference book can never be borrowed, so
+  // `randomizeReferenceBooks()` will always run before `randomizeBorrowedBooks()`
+  randomizeState() {
+    const maxIndex = this.books.length - 1;
+    const referenceBookIndexes = generateRandomNumbersArray(
+      RANDOM_REFERENCE_COUNT,
+      maxIndex,
+    );
+    const borrowedBooksIndexes = generateRandomNumbersArray(
+      RANDOM_BORROWED_COUNT,
+      maxIndex,
+    );
+    this.randomizeReferenceBooks(referenceBookIndexes);
+    this.randomizeBorrowedBooks(borrowedBooksIndexes);
+  }
+
+  private randomizeReferenceBooks(indexes: number[] = []) {
+    if (indexes.length) {
+      indexes.forEach((index) => {
+        this.books[index].isReference = true;
+      });
+    }
+  }
+
+  private randomizeBorrowedBooks(indexes: number[] = []) {
+    if (indexes.length) {
+      indexes.forEach((index) => {
+        this.books[index].isBorrowed = true;
+      });
+    }
   }
 }
