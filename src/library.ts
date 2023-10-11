901e56dad4f51da09170454442f23154392a9bda
Jean <jean@jlouis.me>
2023-10-11 09:31:45 +0100

    Initialize books

--- src/library.ts
+++ src/library.ts
@@ -1,4 +1,17 @@
 import { Book } from './book';
+import * as fs from 'fs';
+
 export class Library {
   private books: Book[] = [];
+
+  constructor(private fileName = '') {
+    if (fileName) {
+      this.initializeLibraryFromFile();
+    }
+  }
+
+  private initializeLibraryFromFile() {
+    const data = fs.readFileSync(this.fileName, 'utf-8');
+    this.books = JSON.parse(data);
+  }
 }
